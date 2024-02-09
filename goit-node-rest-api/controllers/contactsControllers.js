708d5c1198daa1732000dfe8356c3879869e3ace
VictorFil67 <victorfilinovych@gmail.com>
2024-02-09 11:57:38 +0200

    added validateBody

--- goit-node-rest-api/controllers/contactsControllers.js
+++ goit-node-rest-api/controllers/contactsControllers.js
@@ -6,10 +6,6 @@ import {
   updateContactById,
 } from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
-import {
-  createContactSchema,
-  updateContactSchema,
-} from "../schemas/contactsSchemas.js";
 
 export const getAllContacts = async (req, res, next) => {
   try {
@@ -48,10 +44,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
     const { name, email, phone } = req.body;
     const result = await addContact(name, email, phone);
     res.status(201).json(result);
@@ -62,13 +54,8 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    console.log(req.body);
     if (Object.keys(req.body).length === 0) {
       throw HttpError(400, "Body must have at least one field");
-    }
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
     }
     const { id } = req.params;
     const result = await updateContactById(id, req.body);
